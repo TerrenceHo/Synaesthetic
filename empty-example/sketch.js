d7cfe0b352a028fce0aa3c2e1e7b6cc31dcf650c
[Terrence Ho] <terrenceho.books@gmail.com>
2017-02-15 22:53:44 -0800

    Still experimenting with Graphics and masks.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,42 +1,21 @@
-var img;  // Declare variable 'img'.
+var img1;
 var pg;
-var gif;
+
 function preload(){
-    // gif = createImg("assets/binaryText.gif"); 
+    img1 = loadImage("assets/googleZoom.jpg"); 
+    img2 = loadImage("assets/googleMapsImage.png");
 }
-function setup() {
+function setup(){
     createCanvas(720, 400);
-    img = loadImage("assets/googleZoom.jpg");  // Load the image
     pg = createGraphics(720,400);
-    gif = loadImage("assets/binaryText.gif")
+    img1.resize(720, 400);
+    img2.resize(720, 400);
+    img2.filter(GRAY);
+    img1.mask(img2);
 }
 
-function draw() {
-    // image(gif, 200, 200);
-    // pg.noFill();
-    pg.ellipse(360, 200, 340, 340);
-    // Displays the image at its actual size at point (0,0)
-    image(img, 0, 0, width, height);
-    image(pg, 0, 0, width, height);
-    image(gif, 0, 0);
-    // Displays the image at point (0, height/2) at half size
+function draw(){
+    image(img1, 0, 0, width, height);
 }
 
-// function setup(){
-//     createCanvas(720, 400);
-//     // img = loadImage("assets/googleZoom.jpg");
-//     pg = createGraphics(720, 400);
-//     // img.mask(pg);
-// }
 
-// function draw(){
-//     // background(img);
-//     // pg.background(0);
-//     // pg.fill(51);
-//     // pg.noFill();
-//     // pg.ellipse(360, 200, 340, 340);
-//     // // img.mask(pg);
-
-//     image(img, 0, 0, width, height);
-//     // image(pg, 0, 0,width,height);
-// }
